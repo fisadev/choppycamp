ed208e6691e457d730ad7697a7cf61fd306e2869
Sofia Denner <sofi.denner@gmail.com>
2017-03-25 18:48:38 -0300

    fix typos

--- bots/drunk_bot.py
+++ bots/drunk_bot.py
@@ -28,7 +28,7 @@ class DrunkBot(BaseBot):
                 closest_chopp = [chopp, chopp_distance]
 
         if closest_chopp:
-            return utils._a_star(map_, position, closest_chopp[0])
+            return utils.a_star(map_, position, closest_chopp[0])
 
         else:
             return constants.DANCE
