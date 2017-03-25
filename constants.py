1aef4b3223311354fb21c15503420ea79c05d0f1
Juan Pedro Fisanotti <fisadev@gmail.com>
2017-03-25 12:02:21 -0300

    To spanglish

--- constants.py
+++ constants.py
@@ -1,5 +1,13 @@
-PARED = '#'
-BIRRA = 'i'
+WALL = '#'
+CHOPP = 'i'
 LAPTOP = 'L'
 PTYHONISTA1 = 'x'
 PYTHONISTA2 = 'y'
+
+UP = 'up'
+DOWN = 'down'
+LEFT = 'left'
+RIGHT = 'right'
+
+ACTIONS = UP, DOWN, LEFT, RIGHT
+PLAYERS = PYTHONISTA1, PYTHONISTA2
