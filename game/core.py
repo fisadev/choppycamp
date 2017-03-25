57a99a412651859d8f3d4eaa9bbb4d286ab10d12
Ariel Rossanigo <arielrossanigo@gmail.com>
2017-03-25 15:16:00 -0300

    game class moved to core module

--- game/core.py
+++ game/core.py
@@ -1,10 +1,7 @@
 import random
 
-import click
-from constants import (ACTION_DELTAS, ACTIONS, CHOPP, LAPTOP, PLAYER_X,
-                       PLAYER_Y, PLAYERS, SCORE_THINGS, WALL, WON, TIE, LOST)
-from game.map_generator import generate
-from utils import find_thing, get_bot, position_in_map
+from constants import (ACTION_DELTAS, LOST, PLAYER_X, PLAYER_Y, SCORE_THINGS, TIE, WON)
+from utils import find_thing, position_in_map
 
 
 class Game:
@@ -79,29 +76,3 @@ class Game:
         for player_name, player in self.players.items():
             actions.append(player_name, player.act(self.map))
         return actions
-
-
-@click.command()
-@click.option('--max-turns', default=10, help='Max number of game turns.')
-@click.option('--player-x', default='random', help='First player.')
-@click.option('--player-y', default='random', help='Second player.')
-@click.option('--visualizer', default=True, help='Use visualization or not.')
-@click.option('--map', default=None, help='Map to play the game in.')
-def main(player_x, player_y, max_turns, visualizer, map_):
-    if not map_:
-        map_ = generate()
-
-    game = Game(
-        players={
-            PLAYER_X: get_bot(player_x, PLAYER_X, player_y, map_),
-            PLAYER_Y: get_bot(player_y, PLAYER_Y, player_x, map_),
-        },
-        max_turns=max_turns,
-        map_=map_,
-        visualizer=None,
-    )
-    game.play()
-
-
-if __name__ == '__main__':
-    main()
