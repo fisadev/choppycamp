31e0c75852691f7fe80b9db2ef00eb3aec5faa5c
Juan Pedro Fisanotti <fisadev@gmail.com>
2017-03-25 12:28:05 -0300

    Added deltas

--- constants.py
+++ constants.py
@@ -11,3 +11,10 @@ RIGHT = 'right'
 
 ACTIONS = UP, DOWN, LEFT, RIGHT
 PLAYERS = PYTHONISTA1, PYTHONISTA2
+
+ACTION_DELTAS = {
+    UP: (-1, 0),
+    DOWN: (1, 0),
+    LEFT: (0, -1),
+    RIGHT: (0, 1),
+}
