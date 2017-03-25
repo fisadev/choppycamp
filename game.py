3db54b5b73c44f5c36413c49c5e37229beb88f88
Ariel Rossanigo <arielrossanigo@gmail.com>
2017-03-25 12:21:09 -0300

    step and get_actions

--- game.py
+++ game.py
@@ -1,3 +1,15 @@
 class Game:
     def __init__(self):
         pass
+
+    def step(self):
+        actions = self.get_actions_from_players()
+        self.apply_actions(actions)
+        if self.visualizer is not None:
+            self.visualizer.draw(self.map, self.scores)
+
+    def get_actions_from_players(self):
+        actions = []
+        for player in self.players:
+            actions.append(player, player.act(self.map))
+        return actions
