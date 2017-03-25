8d551b245b33329f603597af82849c11bc1a4e26
Juan Pedro Fisanotti <fisadev@gmail.com>
2017-03-25 14:06:23 -0300

    Some advancements in the game loop code

--- game.py
+++ game.py
@@ -1,5 +1,38 @@
+import rnadom
+
+import click
+
+from constants import PLAYERS, ACTIONS, ACTION_DELTAS, WALL, SCORE_THINGS
+from utils import find_thing, position_in_map
+
+
 class Game:
-    def __init__(self):
+    def __init__(self, players, max_turns, map_, visualizer=None):
+        self.map = map_
+        self.players = players
+        self.visualizer = visualizer
+        self.max_turns = max_turns
+
+        self.scores = {}
+
+    def apply_actions(self, actions):
+        random.shuffle(actions)
+
+        for player_id, action in actions:
+            player_r, player_c = find_thing(map_, player_id)
+            delta_r, delta_c = ACTION_DELTAS[action]
+            new_r, new_c = player_r + delta_r, player_c + delta_c
+
+            if position_in_map(map_, (new_r, new_c)):
+                target = self.map[new_r][new_c]
+
+                if target in SCORE_THINGS:
+                    self.scores[player_id] += SCORE_THINGS[target]
+                    self.map[new_r][new_c] = player_id
+                elif not target:
+                    self.map[new_r][new_c] = player_id
+
+    def play(self):
         pass
 
     def step(self):
@@ -13,3 +46,13 @@ class Game:
         for player in self.players:
             actions.append(player, player.act(self.map))
         return actions
+
+
+@click.command()
+def main():
+    game = Game([])
+    game.play()
+
+
+if __name__ == '__main__':
+    main()
