37081dc6c60d8da9d64e9b392ef60f0c2722dfdf
Juan Pedro Fisanotti <fisadev@gmail.com>
2017-03-25 13:45:31 -0300

    More useful constants

--- constants.py
+++ constants.py
@@ -8,13 +8,26 @@ UP = 'up'
 DOWN = 'down'
 LEFT = 'left'
 RIGHT = 'right'
+DANCE = 'dance'
 
-ACTIONS = UP, DOWN, LEFT, RIGHT
+ACTIONS = UP, DOWN, LEFT, RIGHT, DANCE
+
 PLAYERS = PYTHONISTA1, PYTHONISTA2
+SCORE_THINGS = {
+    CHOPP: 1,
+    LAPTOP: 5,
+}
 
 ACTION_DELTAS = {
     UP: (-1, 0),
     DOWN: (1, 0),
     LEFT: (0, -1),
     RIGHT: (0, 1),
+    DANCE: (0, 0),
 }
+
+WON = 'won'
+LOST  = 'lost'
+TIE = 'tie'
+
+RESULTS = WON, LOST, TIE
