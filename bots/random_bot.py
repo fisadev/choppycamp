88c269852c7c2c5a4d86e9bbbe8f1c98681b8bad
Ariel Rossanigo <arielrossanigo@gmail.com>
2017-03-25 15:43:04 -0300

    minor fixes in random bot

--- bots/random_bot.py
+++ bots/random_bot.py
@@ -5,9 +5,10 @@ import random
 
 class RandomBot(BaseBot):
     def _move(self):
-        return RandomBot([constants.UP,
-                          contstants.DOWN,
-                          constants.RIGHT,
-                          constants.LEFT,
-                          constants.DANCE,
-                          ])
+        return random.choice([
+            constants.UP, constants.DOWN, constants.RIGHT, constants.LEFT, constants.DANCE
+        ])
+
+
+def create_bot(map_, id_, other_player):
+    return RandomBot(id_, map_, other_player)
